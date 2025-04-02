79a53a2f71c7a7655a9a18662a5f756dd5a20e20
Atlantis-Gura <97673767+Atlantis-Gura@users.noreply.github.com>
2025-04-02 22:12:25 +0200

    Update search-engine-switcher.user.js
    - Add LINUX DO
    - Change order

--- search-engine-switcher.user.js
+++ search-engine-switcher.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         搜索引擎切换器 · 改二
-// @version      1.3.1
+// @version      1.3.2
 // @description  用于快速切换搜索引擎。有漂亮的高斯模糊外观和深色模式适配。当您滚动网页时，侧栏会自动收起，而当鼠标靠近时，侧栏则会弹出。您可以修改脚本以添加或重新排序搜索引擎。
 // @author       Atlantis-Gura
 // @originAuthor shunz, Corlius
@@ -40,6 +40,12 @@
 // 搜索网址配置
 const urlMapping = [
   {
+    name: "LINUX DO",
+    searchUrl: "https://linux.do/search?q=",
+    keyName: "q",
+    testUrl: /(?:www\.)?linux\.do\/search/i,
+  },
+  {
     name: "V2EX",
     searchUrl: "https://www.google.com/search?q=site:v2ex.com/t%20",
     keyName: "q",
@@ -68,6 +74,12 @@ const urlMapping = [
     searchUrl: "https://www.youtube.com/results?search_query=",
     keyName: "search_query",
     testUrl: /https:\/\/www.youtube.com\/results.*/,
+  },
+  {
+    name: "B站",
+    searchUrl: "https://search.bilibili.com/all?keyword=",
+    keyName: "keyword",
+    testUrl: /https:\/\/search.bilibili.com\/all.*/,
   },
   {
     name: "Google",
@@ -111,12 +123,7 @@ const urlMapping = [
     keyName: "q",
     testUrl: /https:\/\/www.zhihu.com\/search.*/,
   },
-  {
-    name: "B站",
-    searchUrl: "https://search.bilibili.com/all?keyword=",
-    keyName: "keyword",
-    testUrl: /https:\/\/search.bilibili.com\/all.*/,
-  },
+
   {
     name: "小红书",
     searchUrl: "https://www.xiaohongshu.com/search_result?keyword=",
