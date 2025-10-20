cbad9084bda1575efa4b8d6a1b80dea3a41db426
Atlantis-Gura <97673767+Atlantis-Gura@users.noreply.github.com>
2025-10-20 19:09:38 +0200

    Update bilibili-watchlater-simple-redirect.user.js

--- bilibili-watchlater-simple-redirect.user.js
+++ bilibili-watchlater-simple-redirect.user.js
@@ -1,25 +1,25 @@
 // ==UserScript==
 // @name         Bilibili 稍后再看简单重定向
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
-// @description  将稍后再看的链接直接重定向到普通视频页面
+// @version      1.1.0
+// @description  将稍后再看的链接直接重定向到普通视频页面，修复因B站页面加载机制变化导致的失效问题。
 // @author       Atlantis-Gura
-// @homepageURL    https://github.com/Atlantis-Gura/Userscripts
+// @homepageURL  https://github.com/Atlantis-Gura/Userscripts
 // @match        https://www.bilibili.com/list/watchlater*
 // @license      MIT
 // @grant        none
-// @run-at       document-start
+// @run-at       document-end
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    const currentUrl = window.location.href;
-    const watchlaterPattern = /https:\/\/www\.bilibili\.com\/list\/watchlater\?bvid=([A-Za-z0-9]+)/;
-    const match = currentUrl.match(watchlaterPattern);
+    // 更新：使用 URLSearchParams API，比正则表达式更健壮、更易读
+    const urlParams = new URLSearchParams(window.location.search);
+    const bvid = urlParams.get('bvid');
 
-    if (match) {
-        const bvid = match[1];
+    // 如果 URL 中包含 bvid 参数，则立即重定向
+    if (bvid) {
         const newUrl = `https://www.bilibili.com/video/${bvid}/`;
         window.location.replace(newUrl);
     }
